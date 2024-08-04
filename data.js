94cb3244e6fc9037eed0e696e3c328bc0429b823
Diogo Matos <l54466@alunos.uevora.pt>
2024-08-04 17:06:40 +0100

    About Page v1.1

---
+++ data.js
@@ -0,0 +1,52 @@
+export const dataProjects=[
+    {
+        
+    }
+]
+
+export const dataSkills=[
+    {
+        name:"HTML",
+        percentage:"90%"
+    },
+    {
+        name:"CSS",
+        percentage:"85%"
+    },
+    {
+        name:"JavaScript",
+        percentage:"85%"
+    },
+    {
+        name:"React.JS",
+        percentage:"70%"
+    },
+    {
+        name:"Java",
+        percentage:"80%"
+    },
+    {
+        name:"Kotlin",
+        percentage:"75%"
+    },
+    {
+        name:"OutSystems",
+        percentage:"75%"
+    },
+    {
+        name:"Python",
+        percentage:"60%"
+    },
+    {
+        name:"C",
+        percentage:"65%"
+    },
+    {
+        name:"Assembly",
+        percentage:"65%"
+    },
+    {
+        name:"Prolog/OCaml",
+        percentage:"55%"
+    },
+]
--- src/components/Content/AboutContent.jsx
+++ src/components/Content/AboutContent.jsx
@@ -1,20 +1,86 @@
-import "./AboutContent.css";
+import React, { useEffect, useState, useRef } from 'react';
+import './AboutContent.css';
+import { dataSkills } from '../../../data';
 
 function AboutContent() {
+  const [skills, setSkills] = useState(dataSkills); 
+  const skillContainerRef = useRef(null);
+
+  useEffect(() => {
+    const spans = document.querySelectorAll('.bar span');
+    const percentages = document.querySelectorAll('.percentage');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          spans.forEach((span, index) => {
+            const percentage = percentages[index];
+            const width = percentage.textContent;
+            span.style.setProperty('--bar-width', width); 
+            span.classList.add('animate');
+            percentage.style.setProperty('--percentage-left', width); 
+            percentage.classList.add('animate');
+          });
+        } else {
+          spans.forEach((span, index) => {
+            span.classList.remove('animate');
+            const percentage = percentages[index];
+            percentage.classList.remove('animate');
+          });
+        }
+      });
+    });
+
+    if (skillContainerRef.current) {
+      observer.observe(skillContainerRef.current);
+    }
+
+    const updatePercentagePositions = () => {
+      const barWidth = document.querySelector('.bar').clientWidth;
+      percentages.forEach(percentage => {
+        const widthPercentage = parseFloat(percentage.textContent) / 100;
+        percentage.style.left = `${barWidth * widthPercentage-12}px`;
+      });
+    };
+
+    updatePercentagePositions(); 
+
+    window.addEventListener('resize', updatePercentagePositions); 
+
+    return () => {
+      window.removeEventListener('resize', updatePercentagePositions); 
+    };
+  }, [skills]);
+
   return (
     <div className="about-container">
       <div className="about-info">
         <p className="titles">About</p>
-        <p className="paragraph">I'm Diogo Matos. I'm 21 years old and recently graduated with a degree in
-            Computer Sciences Engineering from Évora, Portugal. I am from Portugal and am highly proactive, 
-            always eager to learn and work. My main focus is on web and mobile development.
+        <p className="paragraph">
+          I'm Diogo Matos. I'm 21 years old and recently graduated with a degree in
+          Computer Sciences Engineering from Évora, Portugal. I am from Portugal and am highly proactive, 
+          always eager to learn and work. My main focus is on web and mobile development.
         </p>
       </div>
       <div className="projects">
         <p className="titles">Projects</p>
-      </div> 
-      <div className="skills">
-          <p className="titles">Skills</p>
+        {/* Future projects content can go here */}
+      </div>
+      <div className="skills" ref={skillContainerRef}>
+        <p className="titles">Skills</p>
+        <div className="skills-header">
+          {skills.map(skill => (
+            <div key={skill.name} className="skill-container">
+              <p>{skill.name}</p>
+              <div className={`percentage ${skill.name.toLowerCase()}-percentage`}>
+                {skill.percentage}
+              </div>
+              <div className="bar">
+                <span className={`${skill.name.toLowerCase()}`}></span>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
